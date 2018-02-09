7be518950ae13fbfaccfff4dcf7244596d94052f
dire16 <270576975@qq.com>
2018-02-09 12:04:04 +0800

    learn day4

--- src/main/java/com/mmall/service/imp/UserServiceImpl.java
+++ src/main/java/com/mmall/service/imp/UserServiceImpl.java
@@ -5,11 +5,14 @@ import com.mmall.dao.UserMapper;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service("iUserService")
 public class UserServiceImpl implements IUserService {
 
     @Autowired
     private UserMapper userMapper;
+
     @Override
     public ServerResponse<User> login(String username, String password) {
         int resultCount=userMapper.checkUsername(username);
@@ -17,6 +20,12 @@ public class UserServiceImpl implements IUserService {
             return ServerResponse.createByErrorMessage("用户名不存在");
         }
         //todo 密码登录MD5
-        return null;
+
+        User user =userMapper.selectLogin(username,password);
+        if(user == null) {
+            return ServerResponse.createByErrorMessage("密码错误");
+        }
+        user.setPassword(org.apache.commons.lang3.StringUtils.EMPTY);
+        return ServerResponse.createBySuccess("登录成功",user);
     }
 }
