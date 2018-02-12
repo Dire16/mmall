a466889eac249d71ee8109d37497ad9efe928f6e
dire16 <270576975@qq.com>
2018-02-12 19:47:26 +0800

    learn day7

--- src/main/java/com/mmall/common/TokenCache.java
+++ src/main/java/com/mmall/common/TokenCache.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 
 public class TokenCache {
     private static Logger logger = LoggerFactory.getLogger(TokenCache.class);
-
+    public static final String TOKEN_PREFIX="token_";
     private static LoadingCache<String,String> localCache = CacheBuilder.newBuilder().initialCapacity(1000).maximumSize(10000).expireAfterAccess(12, TimeUnit.HOURS)
             .build(new CacheLoader<String, String>() {
                 @Override
--- src/main/java/com/mmall/service/imp/UserServiceImpl.java
+++ src/main/java/com/mmall/service/imp/UserServiceImpl.java
@@ -93,9 +93,76 @@ public class UserServiceImpl implements IUserService {
         int resultCount=userMapper.checkAnswer(username,question,answer);
         if(resultCount>0){
             String forgetToken= UUID.randomUUID().toString();
-            TokenCache.setKey("token_"+username,forgetToken);
+            TokenCache.setKey(TokenCache.TOKEN_PREFIX+username,forgetToken);
             return ServerResponse.createBySuccess(forgetToken);
         }
         return ServerResponse.createByErrorMessage("问题的答案错误");
+    }
+
+    public ServerResponse<String> forgetResetPassword(String username,String passwordNew,String forgetToken){
+        if(StringUtils.isNotBlank(forgetToken)){
+            return ServerResponse.createByErrorMessage("参数错误，token需要传递");
+        }
+        ServerResponse validResponse=this.checkValid(username,Const.USERNAME);
+        if(validResponse.isSuccess()){
+            return ServerResponse.createByErrorMessage("用户不存在");
+        }
+        String token =TokenCache.getKey(TokenCache.TOKEN_PREFIX+username);
+        if(StringUtils.isNotBlank(token)){
+            return ServerResponse.createByErrorMessage("token无效或者过期");
+        }
+        if(StringUtils.equals(forgetToken,token)){
+            String md5Password=MD5Util.MD5EncodeUtf8(passwordNew);
+            int rowCount =userMapper.updatePasswordByUsername(username,md5Password);
+            if(rowCount>0){
+                return ServerResponse.createBySuccessMessage("修改密码成功");
+            }else {
+                return ServerResponse.createByErrorMessage("token错误，请重新获取重置密码的token");
+
+            }
+        }
+        return ServerResponse.createByErrorMessage("修改密码失败");
+    }
+
+    public ServerResponse<String> resetPassword(String passwordOld,String passwordNew,User user){
+        int resultCount=userMapper.checkPassword(MD5Util.MD5EncodeUtf8(passwordOld),user.getId());
+        if(resultCount == 0){
+            return ServerResponse.createByErrorMessage("旧密码错误");
+        }
+        user.setPassword(MD5Util.MD5EncodeUtf8(passwordNew));
+        int updateCount=userMapper.updateByPrimaryKeySelective(user);
+        if(updateCount>0){
+            return ServerResponse.createBySuccessMessage("密码更新成功");
+        }
+        return ServerResponse.createByErrorMessage("密码更新失败");
+
+    }
+
+    public ServerResponse<User> updateInformation(User user){
+        int resultCount=userMapper.checkEmailByUserId(user.getEmail(),user.getId());
+        if(resultCount>0){
+            return ServerResponse.createByErrorMessage("email已存在，请更换email再尝试更新");
+        }
+        User updateUser =new User();
+        updateUser.setId(user.getId());
+        updateUser.setEmail(user.getEmail());
+        updateUser.setPhone(user.getPhone());
+        updateUser.setQuestion(user.getQuestion());
+        updateUser.setAnswer(user.getAnswer());
+
+        int updateCount=userMapper.updateByPrimaryKeySelective(updateUser);
+        if(updateCount>0){
+            return ServerResponse.createBySuccess("更新个人信息成功",updateUser);
+        }
+        return ServerResponse.createByErrorMessage("更新个人信息失败");
+    }
+
+    public ServerResponse<User> getInformation(Integer userId){
+        User user=userMapper.selectByPrimaryKey(userId);
+        if(user==null){
+            return ServerResponse.createByErrorMessage("找不到当前用户");
+        }
+        user.setPassword(StringUtils.EMPTY);
+        return ServerResponse.createBySuccess(user);
     }
 }
