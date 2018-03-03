cd6094e323760fc4809ba176d22f916647bbdc2f
dire16 <270576975@qq.com>
2018-03-04 00:00:16 +0800

    learn day10

--- src/main/java/com/mmall/controller/backend/CategoryManageController.java
+++ src/main/java/com/mmall/controller/backend/CategoryManageController.java
@@ -54,4 +54,19 @@ public class CategoryManageController {
             return ServerResponse.createByErrorMessage("无权限操作，需要管理员权限");
         }
     }
+
+    @RequestMapping("get_category.do")
+    @ResponseBody
+    public ServerResponse getChildrenParallelCategory(HttpSession session,Integer categoryNameId){
+        User user =(User)session.getAttribute(Const.CURRENT_USER);
+        if(user==null){
+            ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录，请重新登录");
+        }
+        if(iUserService.checkAdminRole(user).isSuccess()){
+            //是管理员
+            return iCategoryService.getChildrenParallelCategory(categoryNameId);
+        }else {
+            return ServerResponse.createByErrorMessage("无权限操作，需要管理员权限");
+        }
+    }
 }
--- src/main/java/com/mmall/service/ICategoryService.java
+++ src/main/java/com/mmall/service/ICategoryService.java
@@ -1,10 +1,15 @@
 package com.mmall.service;
 
 import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Category;
+
+import java.util.List;
 
 public interface ICategoryService {
 
     ServerResponse addCategory(String categoryName, Integer parentId);
 
     ServerResponse updateCategoryName(Integer categoryNameId,String categoryName);
+
+    ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId);
 }
